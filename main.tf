fda6388ae30858fa3365f82ddeb308c3315f4f32
saimaali-tech <saimaalisaima313@gmail.com>
2026-04-24 00:37:24 +0500

    updated main.tf file

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ module "eks" {
   eks_managed_node_groups = {
     general = {
       name           = "general-ng"
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       min_size       = 2
       max_size       = 4
       desired_size   = 2
